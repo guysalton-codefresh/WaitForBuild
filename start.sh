aabda732469b1b0be204305bfc1283aebdc5d70a
Guy Salton <47520615+guysalton-codefresh@users.noreply.github.com>
2019-11-12 15:59:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "the context of my CLI is:"
+codefresh auth current-context
+
 BUIL_ID=$(codefresh get build --pipeline-name=${PIPELINE_NAME} --revision ${CF_REVISION} -o json -l 1 | jq -r .id)
-bash -c "while true; do echo Waiting for build https://g.codefresh.io/build/$BUIL_ID && sleep 30; done &"  
+
+echo ${PIPELINE_NAME}
+
+while true; do echo Waiting for build https://g.codefresh.io/build/$BUIL_ID && sleep 30; done &"
+
 codefresh wait $BUIL_ID || true
+
 cf_export BUILD_PASSED=$(codefresh get build $BUIL_ID -o json | jq -r .status)
