d442abadc75d34ade578f14aff66367c793f8155
Guy Salton <47520615+guysalton-codefresh@users.noreply.github.com>
2019-11-12 12:48:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUIL_ID=$(codefresh get build --pipeline-name=guysalton-codefresh/docker-based-pipelines-webinar/aws-kiam-venona --revision ${{CF_REVISION}} -o json -l 1 | jq -r .id)
+BUIL_ID=$(codefresh get build --pipeline-name=${PIPELINE_NAME} --revision ${CF_REVISION} -o json -l 1 | jq -r .id)
 bash -c "while true; do echo Waiting for build https://g.codefresh.io/build/$BUIL_ID && sleep 30; done &"  
 codefresh wait $BUIL_ID || true
 cf_export BUILD_PASSED=$(codefresh get build $BUIL_ID -o json | jq -r .status)
